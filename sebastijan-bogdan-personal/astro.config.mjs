962033dccdbb3a50b0474b71bce78fc09a4b3c28
FunnySkillz <sebastijan.bogdan@outlook.com>
2026-04-15 20:03:16 +0200

    config: enable vercel adapter and sitemap integration

--- sebastijan-bogdan-personal/astro.config.mjs
+++ sebastijan-bogdan-personal/astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  site: 'https://sebastijanbogdan.com',
+  site: "https://sebastijanbogdan.com",
+  adapter: vercel(),
+  integrations: [sitemap()],
   i18n: {
-    defaultLocale: 'de',
-    locales: ['de', 'en']
+    defaultLocale: "de",
+    locales: ["de", "en"]
   }
 });
