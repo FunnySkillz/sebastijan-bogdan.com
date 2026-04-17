d57a34c736fbefbfede58927f049fc725382c65e
FunnySkillz <sebastijan.bogdan@outlook.com>
2026-04-17 13:38:23 +0200

    config: set canonical site domain to sebastijan-bogdan.com

--- sebastijan-bogdan-personal/astro.config.mjs
+++ sebastijan-bogdan-personal/astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  site: "https://sebastijanbogdan.com",
+  site: "https://sebastijan-bogdan.com",
   adapter: vercel(),
   integrations: [sitemap()],
   i18n: {
